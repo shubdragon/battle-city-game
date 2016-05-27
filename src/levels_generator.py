5ec37548efc630c8c63550a12b573e150027df79
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2016-05-27 14:15:29 -0500

    Created world 1 database

--- src/levels_generator.py
+++ src/levels_generator.py
@@ -10,7 +10,28 @@ if __name__ == '__main__':
     # Open connection to db file
     db = directory + '/' + 'levels.db'
     connection = sqlite3.connect(db)
+    c = connection.cursor()
 
     # Transform data from file to data in sql
+    command = ''
+    line_edit = ''
+    table_name = ''
     for line in source:
-        print line
+        if line[0] == ':':
+            table_name = line[1:].replace('\n', '')
+            command = 'CREATE TABLE IF NOT EXISTS "' + table_name + '"('
+            for i in range(1, 19):
+                command += 'cell_' + str(i) + ' CHARACTER(4), '
+            command += 'cell_19 CHARACTER(4))'
+            c.execute(command)
+        if line[0] == ' ':
+            line_edit = line.replace('\n', '')
+            command = 'INSERT INTO "' + table_name + '" VALUES('
+            for i in line_edit.split(','):
+                command += '"' + i + '",'
+            command = command[0:-1]
+            command += ')'
+            c.execute(command)
+    connection.commit()
+    c.close()
+    connection.close()
